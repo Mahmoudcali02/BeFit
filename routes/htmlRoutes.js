e26d88386e2e121bb96b7952aec028c46b93f1c1
Mahmoud Cali <mahmoudcali2@gmail.com>
2022-01-22 22:06:44 +0000

    made htmlRoutes file

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,7 +1,23 @@
 const router = require('express').Router();
+const {Exercise} = require('../models')
+const withAuth = require("../utils/auth")
 router.get("/", function (req, res) {
     res.render("main")
 });
+
+router.get("/dashboard", withAuth, async function (req, res) {
+    try {
+        const exerciseData = await Exercise.findAll({ 
+            where: {
+                userId: req.session.userId
+            }
+        });
+        const exercises = exerciseData.map((exercise) => exercise.get({ plain: true}));
+        res.render("dashboard", exercises);
+    } catch (error) {
+        
+    }
+})
 
 router.get("/login", function (req, res) {
     if (req.session.loggedIn){
