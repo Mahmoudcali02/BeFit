5504e5c0dc1c1fd67f32f2e7d1b1882dfe73b951
Mahmoud Cali <mahmoudcali2@gmail.com>
2022-01-22 21:47:44 +0000

    created the log out handler

--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -51,3 +51,13 @@ router.post('/', async (req, res) => {
       res.status(400).json({ message: 'No user account found!' });
     }
   });
+
+  router.post('/logout', (req, res) => {
+    if (req.session.loggedIn) {
+      req.session.destroy(() => {
+        res.status(204).end();
+      });
+    } else {
+      res.status(404).end();
+    }
+  });
