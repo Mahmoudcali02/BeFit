ed2b2ac04c5667a62926e59751bcd31f3b419ca7
Mahmoud Cali <mahmoudcali2@gmail.com>
2022-01-22 21:32:07 +0000

    cleared the routes folder

--- routes/api/index.js
+++
@@ -1,9 +0,0 @@
-const router = require('express').Router();
-const loginRoutes = require('./loginRoutes');
-const newUser = require('./newUser.js')
-
-
-router.use('/login', loginRoutes);
-router.use("/dashboard", newUser);
-
-module.exports = router;
