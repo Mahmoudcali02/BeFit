71bc611e20ea5dd277e0131da0956d296b881ca2
Mahmoud Cali <mahmoudcali2@gmail.com>
2022-01-22 20:45:54 +0000

    created exercise model

--- models/Exercise.js
+++ models/Exercise.js
@@ -1,2 +1,38 @@
 const {Model, DataTypes} = require ("sequelize");
 const sequelize = require ("../config/connection");
+
+class Exercise extends Model {}
+Exercise.init ({
+    id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true,
+    },
+    name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    sets: {
+        dataType: DataTypes.INTEGER,
+        allowNull: true,
+    },
+    reps: {
+        dataType: DataTypes.INTEGER,
+        allowNull: true,
+    },
+    minutes: {
+        dataType: DataTypes.INTEGER,
+        allowNull: true,
+    }
+},
+{
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: "Exercise"
+}
+)
+
+module.exports = Exercise
